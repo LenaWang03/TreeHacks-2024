2ea69bd90e8f7edf3918a43373ad2c5424b95d04
Gordon Cheung <georgehwc@gmail.com>
2024-02-17 00:22:08 -0800

    initial implementation done

---
+++ client/src/components/DefaultPrompt/DefaultPrompt.tsx
@@ -0,0 +1,9 @@
+import "./DefaultPrompt.css";
+
+export default function DefaultPrompt() {
+  return (
+    <div className="prompt-box">
+      <h1 className="prompt-text">Send an email</h1>
+    </div>
+  );
+}
